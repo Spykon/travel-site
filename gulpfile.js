2ad47f113c5d5a97eaa1e82ad2c1e122fef81a1a
Elvis <elvis.kondi@yahoo.it>
2019-01-18 13:45:09 +0100

    Completed large hero block and intro content block.

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+
+
+/* QUESTA PARTE E' STATA CREATA SLO A TITOLO DI ESERCIZIO
+gulp.task('default', function() {
+	console.log("Hooray - you created a Gulp task.");
+});
+
+gulp.task('html', function() {
+	console.log("Imagine something useful being done to your HTML here.");
+});
+*/
+
+/*gulp.task('styles', function() {
+	console.log("Imagine Sass or PostCSS tasks running here.");
+});*/
+
+
+
+
